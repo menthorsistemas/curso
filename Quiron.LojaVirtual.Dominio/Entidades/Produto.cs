e5b8268fac36ca69a791d972582a55490e420ed0
menthorsistemas <fernando.menthorsistemas@gmail.com>
2014-12-12 10:12:32 -0200

    Teste em 12.12.2014

--- Quiron.LojaVirtual.Dominio/Entidades/Produto.cs
+++ Quiron.LojaVirtual.Dominio/Entidades/Produto.cs
@@ -11,7 +11,7 @@ namespace Quiron.LojaVirtual.Dominio.Entidades
         public int ProdutoID { get; set; }
         public string Nome { get; set; }
         public string Descricao { get; set; }
-        public decimal Preco { get; set; }
         public int Categoria { get; set; }
+        public decimal Preco { get; set; }
     }
 }
